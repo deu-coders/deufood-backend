e1ba5cf5fd950270dad94543a5c1ebd7a96aeda5
solo5star <solo_5@naver.com>
2021-12-06 17:48:25 +0000

    implement Top 5 Foods (by rate)

--- foods/urls.py
+++ foods/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path,include 
 from rest_framework_nested import routers 
-from .views import FoodViewSet, FoodReviewViewSet, FoodCategoryViewSet
+from .views import Best5FoodViewSet, FoodViewSet, FoodReviewViewSet, FoodCategoryViewSet
 
 router = routers.DefaultRouter()
 router.register('foods', FoodViewSet, basename='food')
 router.register('food_categories', FoodCategoryViewSet, basename='food_categories')
+router.register('best_foods', Best5FoodViewSet, basename='best_foods')
 
 reviews_router = routers.NestedDefaultRouter(router, 'foods', lookup='food')
 reviews_router.register('reviews', FoodReviewViewSet, basename='review')
--- foods/views.py
+++ foods/views.py
@@ -1,5 +1,7 @@
 from django.conf import settings
+from django.db.models import Count, F
 from rest_framework import viewsets, mixins
+from rest_framework.response import Response
 from .serializers import FoodCategoryRetreiveSerializer, FoodCategorySerializer, FoodListSerializer, FoodRetreiveSerializer, FoodSerializer, FoodReviewSerializer
 from .models import FoodCategory, Food, FoodReview, RateTypes
 
@@ -46,3 +48,28 @@ class FoodReviewViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         food = Food.objects.get(pk=self.kwargs['food_pk'])
         serializer.save(author=self.request.user, food=food)
+
+
+class Best5FoodViewSet(viewsets.ViewSet):
+
+    def list(self, request):
+        results = FoodReview.objects \
+            .filter(rate=RateTypes.호) \
+            .select_related('food') \
+            .values('food') \
+            .annotate(likes=Count('rate')) \
+            .values(
+                id=F('food__id'),
+                category=F('food__category'),
+                name=F('food__name'),
+                description=F('food__description'),
+                price=F('food__price'),
+                thumbnail=F('food__thumbnail'),
+                likes=F('likes'),
+            ) \
+            .order_by('-likes')[:5]
+
+        for result in results:
+            result['thumbnail'] = request.build_absolute_uri(settings.MEDIA_URL + result['thumbnail'])
+
+        return Response(results)
